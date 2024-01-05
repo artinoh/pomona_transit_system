8599cfb9d0a442e29224b481a3a14b6d87d2b8e3
Artin Ohanessian <artinohanessian@gmail.com>
2024-01-04 23:48:47 -0800

    Fixed .gitignore

--- build/CMakeFiles/pomona_transit_system.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for pomona_transit_system.
--- build/CMakeFiles/pomona_transit_system.dir/transit_menu/src/transit_menu.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/pomona_transit_system.dir/transit_menu/src/transit_menu.cpp.o: \
-  /Users/artin/CS4350/lab4/pomona_transit_system/transit_menu/src/transit_menu.cpp
